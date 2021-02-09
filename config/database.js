e45517608ab8cfca1ec89598871f9b9f68489caf
aegirorn <41763474+aegirorn@users.noreply.github.com>
2021-02-09 18:10:28 +0000

    database changed to postgress

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi-books"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "strumpur"),
+        schema: env("DATABASE_SCHEMA", "public"), // Not Required
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
